21b4dae4dc5fc36951130b05533d4ea0907b137b
Eugene Hp <eugene.hp2012@gmail.com>
2014-02-02 22:55:25 +0100

    fix

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ var client = SNSClient(auth, function(err, message) {
 });
 
 app.post('/receive', client);
+var test = function(req,res){
+	console.log(req.body);
+	res.end('.');
+};
+
+app.get('*',test);
+app.post('*',test);
 
 app.listen(config.port);
 console.log('SNS Endpoint is listenning',config.port,'port');
